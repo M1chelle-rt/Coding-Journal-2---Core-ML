164163627d9bf4c45ca3299b909b9bbe7deea327
Michelle Thomas <michellerose.thms@gmail.com>
2025-04-21 12:22:34 -0500

    Initial Commit

---
+++ Coding Journal 2 - Core ML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coding Journal 2 - Core ML
+//
+//  Created by Michelle Thomas on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
